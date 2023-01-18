c0e6f1f3381a6b0ab843ddfe089f56211af50e1a
VictorUzunov <vitouzunov@gmail.com>
2023-01-18 22:24:45 +0200

    Final Project

--- main.cpp
+++ main.cpp
@@ -176,3 +176,130 @@ void letters(string &availableLetters) {
     }
     cout << endl;
 }
+void startTheGame(int lettersInEachRound, int rounds, int &points, vector<string> &words) {
+    int round = 0;
+    while (round != rounds) {
+        cout << "Available letters in round " << round + 1 << " are:" << endl;
+        string availableLetters;
+        randomLettersGenerator(availableLetters, lettersInEachRound);
+        cout << "1.Change one of the letters(you will lose 1 point)" << endl;
+        cout << "2.Skip the round and get new letters" << endl;
+        string yourWord;
+        bool again = true;
+        string option1 = "1";
+        string option2 = "2";
+        do {
+            cin >> yourWord;
+            //Check if the word is valid and whether it is in the dictionary
+            if (correctWordForTheRound(availableLetters, yourWord) && wordExistsInDictionary(words, yourWord)) {
+                points += (int) yourWord.size();
+                again = false;
+                if (round + 1 != rounds) {
+                    cout << "Your points so far are:" << points << endl;
+                } else {
+                    cout << "Your total points are:" << points << endl;
+                }
+            }
+                //The action if you want to change one letter from the available
+            else if (yourWord == option1) {
+                points--;
+                cout << "Enter the letter from the available ones that you want to change:";
+                char letter;
+                cin >> letter;
+                changeAvailableLetter(availableLetters, letter);
+                cout << "Available letters in round " << round + 1 << " are:" << endl;
+                letters(availableLetters);
+                cout << "1.Change one of the letters(you will lose 1 point)" << endl;
+                cout << "2.Skip the round and get new letters" << endl;
+            }
+                //Skip the round
+            else if (yourWord == option2) {
+                break;
+            }
+                //If you have entered invalid word or word that is not in the dictionary
+            else {
+                cout << "Invalid word. Try again with: ";
+                letters(availableLetters);
+                cout << "1.Change one of the letters(you will lose 1 point)" << endl;
+                cout << "2.Skip the round and get new letters" << endl;
+            }
+        } while (again);
+        round++;
+    }
+}
+
+int main() {
+    //Information about the game
+    cout << "Welcome to the Scrabble Game" << endl;
+    cout << "Before you start the game read the instructions carefully:" << endl;
+    cout << "In each round are generated letters and you should make a word." << endl;
+    cout << "For correct word you receive as many points as the number of" << endl;
+    cout << "letters int it. If you cannot do it you can change one of the" << endl;
+    cout << "letters and get a new one but every time you will lose one point." << endl;
+    cout << "You have also the option to skip the whole round and get 10 new letters." << endl;
+    cout << "After the last round you will see your total points." << endl;
+    cout << "Good luck!" << endl;
+    int points = 0;
+    int lettersInEachRound = 10;
+    int rounds = 10;
+    vector<string> words;
+    bool success = readDictionary(words);
+    if (!success) {
+        cerr << "The file could not be opened";
+        return 1;
+    }
+    readDictionary(words);
+    bool keepGoing = true;
+    //Returning to menu cycle
+    while (keepGoing) {
+        cout << endl;
+        cout << "MENU:" << endl;
+        cout << "1.Start the game" << endl;
+        cout << "2.Settings" << endl;
+        cout << "3.Add new word to the Dictionary" << endl;
+        cout << "4.Exit" << endl;
+        cout << "Input 1, 2, 3 or 4:";
+        string input;
+        do {
+            cin >> input;
+            if (!correctInput(input) || !cin.good()) {
+                cout << "Incorrect input, try again:" << endl;
+            }
+        } while (!correctInput(input));
+        switch (input[0]) {
+            //Start the game
+            case '1': {
+                startTheGame(lettersInEachRound, rounds, points, words);
+            }
+                break;
+                //Settings
+            case '2': {
+                settings(lettersInEachRound, rounds);
+            }
+                break;
+                //Add new word
+            case '3': {
+                cout << "You can add a new word in the dictionary." << endl;
+                cout << "Input your word:";
+                string newWord;
+                cin >> newWord;
+                if (validNewWord(newWord)) {
+                    cout << "Your word was added" << endl;
+                    addWordToDictionary(words, newWord);
+                } else {
+                    cout << "This word does not exist!";
+                    cout << endl;
+                }
+            }
+                break;
+                //Exit
+            case '4': {
+                keepGoing = false;
+            }
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
